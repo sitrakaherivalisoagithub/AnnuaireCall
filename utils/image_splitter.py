42dff2633eadb3fc054ecab8e663c7d4938b988a
Sitraka HERIVALISOA <s.herivalisoa@etech.local>
2024-05-15 14:29:43 +0300

    refactor sophistical algo

--- utils/image_splitter.py
+++ utils/image_splitter.py
@@ -4,95 +4,77 @@ from sklearn.cluster import DBSCAN
 
 
 class ImageSplitter:
-
     def __init__(self, filename: str, naive_method: bool = False):
         self.image = cv2.imread(filename)
         self.inverted_gray_image = None
         self.gaps = []
         self.naive_method = naive_method
 
-    def gray_scale_image(self):
+    def convert_to_gray(self):
         """Convert the image to grayscale"""
         gray_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-        # Invert the grayscale image
         self.inverted_gray_image = cv2.bitwise_not(gray_image)
+        self._ignore_border()
 
-        # Ignore border
+    def _ignore_border(self):
+        """Ignore border pixels in the grayscale image"""
         margin = self.inverted_gray_image.shape[1] // 10
         y_max = self.inverted_gray_image.shape[1]
-
         self.inverted_gray_image[:, 0:margin] = 255
         self.inverted_gray_image[:, y_max - margin: y_max] = 255
 
     def split_image(self):
-        self.gray_scale_image()
+        self.convert_to_gray()
 
         if self.naive_method:
-            self.naive_algorithm()
+            self._naive_algorithm()
         else:
-            self.sophisticated_algorithm()
+            self._sophisticated_algorithm()
 
-    def sophisticated_algorithm(self):
-
+    def _sophisticated_algorithm(self):
         sorted_array = np.sort(self.inverted_gray_image.sum(axis=0))
         index_sorted_array = np.argsort(self.inverted_gray_image.sum(axis=0))
-
-        # Index Array
         data = index_sorted_array[(sorted_array / self.inverted_gray_image.shape[0]) < 1]
-
-        # Réorganiser les données en colonne
         X = data.reshape(-1, 1)
-
-        # Utiliser DBSCAN pour trouver les clusters
         dbscan = DBSCAN(eps=10, min_samples=2)
         dbscan.fit(X)
-
-        # Obtenir les labels des clusters
         labels = dbscan.labels_
-
-        # Extraire les valeurs uniques de chaque cluster
         unique_labels = np.unique(labels)
-
         groups = {}
-        # Afficher les blocs trouvés
         for label in unique_labels:
             if label == -1:
                 print("Noise:", data[labels == label])
             else:
-                groups["Group{}".format(label)] = data[labels == label]
+                groups[f"Group{label}"] = data[labels == label]
+        self._calculate_gaps(groups)
+        self._save_images()
 
+    def _calculate_gaps(self, groups):
         for value in groups.values():
             self.gaps.append(int(np.median(value)))
-
-        # important
         self.gaps = sorted(self.gaps)
 
+    def _save_images(self):
         images = []
         previous_left = 0
-
         for i, gap in enumerate(self.gaps):
             images.append(self.image[:, previous_left: gap])
             previous_left = gap
         images.append(self.image[:, previous_left: self.inverted_gray_image.shape[1]])
+        for i, img in enumerate(images):
+            cv2.imwrite(f"image{i+1}.jpg", img)
 
-        # save image
-        for i, im in enumerate(images):
-            cv2.imwrite('image' + str(i+1) + '.jpg', im)
-
-    def naive_algorithm(self):
+    def _naive_algorithm(self):
         y_max = self.inverted_gray_image.shape[1]
-        line_median = self.inverted_gray_image.shape[1] // 2
+        line_median = y_max // 2
         first_gap = np.argmin(self.inverted_gray_image[:, 0:line_median].sum(axis=0))
-        second_gap = np.argmin(self.inverted_gray_image[:, line_median: y_max].sum(axis=0))
+        second_gap = np.argmin(self.inverted_gray_image[:, line_median:y_max].sum(axis=0)) + line_median
+        self._save_individual_images(first_gap, second_gap)
 
-        second_gap = second_gap + line_median
+    def _save_individual_images(self, first_gap, second_gap):
         image1 = self.image[:, 0: first_gap]
         image2 = self.image[:, first_gap: second_gap]
         image3 = self.image[:, second_gap: self.image.shape[1]]
-
-        # save image
         cv2.imwrite('image1.jpg', image1)
         cv2.imwrite('image2.jpg', image2)
         cv2.imwrite('image3.jpg', image3)
-
-        # return image1, image2, image3
